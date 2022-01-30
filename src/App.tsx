251bc0af3fc280c4b9e90b91cb8b9f47ec7c25cf
iamidlek <iamidlek@naver.com>
2022-01-31 01:33:15 +0900

    feat: Set Componets in App.tsx - #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Box } from "@mui/material";
+import { useState } from "react";
+import TopAppBar from "./components/TopAppBar";
+import LeftGNB from "./components/LeftGNB";
+import Main from "./components/Main";
+
 function App() {
-  return <div className="App">d</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <TopAppBar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <LeftGNB isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Main />
+    </Box>
+  );
 }
 
 export default App;
