9dd92511a24af3d433f9e4248400f7229f735164
iamidlek <iamidlek@naver.com>
2022-02-01 00:36:36 +0900

    feat: initial settings

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,3 +4,19 @@ export interface MenuProps {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
+
+export interface MenuItemProps {
+  title: string;
+  onClick?: () => void;
+  path?: string;
+  children?: React.ReactChild;
+}
+
+export interface GridItemProps {
+  das: number;
+  children?: React.ReactChild;
+}
+
+export interface ChangeBtn {
+  setIsDelete: Dispatch<SetStateAction<boolean>>;
+}
