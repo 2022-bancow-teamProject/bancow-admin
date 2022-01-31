61e084c0086576f648868aa5f8330850d254bc65
iamidlek <iamidlek@naver.com>
2022-02-01 00:39:03 +0900

    feat: drawerWidth controll

--- src/components/LeftGNB.tsx
+++ src/components/LeftGNB.tsx
@@ -1,21 +1,19 @@
+import { useState } from "react";
 import {
   Box,
   Divider,
   IconButton,
   List,
-  ListItem,
-  ListItemText,
   Toolbar,
   Collapse
 } from "@mui/material";
 import { ChevronLeft, ExpandLess, ExpandMore } from "@mui/icons-material";
 import { MenuProps } from "../interfaces";
-import { useState } from "react";
-
-const drawerWidth = 240;
+import { drawerWidth } from "../theme";
+import MenuItem from "./MenuItem";
 
 const LeftGNB: React.FC<MenuProps> = ({ isOpen, setIsOpen }) => {
-  const [isDrop, setIsDrop] = useState(false);
+  const [isDrop, setIsDrop] = useState(true);
   const handleClick = () => {
     setIsDrop((state) => !state);
   };
@@ -50,26 +48,22 @@ const LeftGNB: React.FC<MenuProps> = ({ isOpen, setIsOpen }) => {
           borderRight: "1px solid #AAAAB1"
         }}
       >
-        <ListItem button onClick={handleClick}>
-          <ListItemText primary="Inbox" />
+        <MenuItem title="컨텐츠 관리" onClick={handleClick}>
           {isDrop ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
+        </MenuItem>
         <Collapse in={isDrop} timeout="auto" unmountOnExit>
           <List component="div" disablePadding sx={{ pl: 4 }}>
-            <ListItem button>
-              <ListItemText primary="Starred" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="Starred" />
-            </ListItem>
+            <MenuItem title="구매자 리뷰" path="review"></MenuItem>
+            <MenuItem title="농가 소개 정보" path="farmer"></MenuItem>
+            <MenuItem title="공지" path="notice"></MenuItem>
+            <MenuItem title="언론 보도" path="news"></MenuItem>
+            <MenuItem title="이벤트" path="event"></MenuItem>
+            <MenuItem title="FAQ" path="faq"></MenuItem>
           </List>
         </Collapse>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} sx={{ whiteSpace: "nowrap" }} />
-            </ListItem>
-          ))}
+          <MenuItem title="QnA" path="qna"></MenuItem>
+          <MenuItem title="팝업" path="popup"></MenuItem>
         </List>
         <Divider />
       </Box>
