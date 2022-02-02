b6cd75b3c33199cbd35a51ee483125220b462fbf
iamidlek <iamidlek@naver.com>
2022-02-02 13:29:56 +0900

    feat: add btn

--- src/components/button/Add.tsx
+++ src/components/button/Add.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
+import { ChangeBtn } from "../../interfaces";
 
-const Add = () => {
+const Add: React.FC<ChangeBtn> = ({ func }) => {
   return (
     <Button
       variant="contained"
       sx={{ width: 100, marginRight: 3, fontSize: 16, fontWeight: 700 }}
+      onClick={func}
     >
       추가
     </Button>
